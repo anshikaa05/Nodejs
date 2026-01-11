9e65c4eae02a4daa2b4e2f95f37977fbce7c391f
Anshika Srivastava <anshika.srivastava0506@gmail.com>
2026-01-11 11:16:42 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("Hello Everyone");
-});
-
-app.use("/dashboard",(req,res)=>{
-    res.send("Dashboard ");
+app.use("/user", [(req,res,next) =>{
+    console.log("HELLO11");
+    next();
+    // res.send("HELLO");
+}],
+(req,res,next)=>{
+    res.send("HELLO2");
 })
+
+// app.use("/",(req,res)=>{
+//     res.send("Hello Everyone");
+// });
+
+// app.get("/dashboard",(req,res)=>{
+//     res.send("Dashboard ");
+// })
+// app.post("/test",(req,res)=>{
+//     res.send("POST request");
+// })
 
 app.listen('7777', ()=>{
     console.log("Connected to server 7777....");
